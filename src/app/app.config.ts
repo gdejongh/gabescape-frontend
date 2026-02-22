e4d937c6a05f15bafd325edb59d6fb98727f6d42
Gabe <dejonghgabe1@gmail.com>
2026-02-22 13:04:42 -0500

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,17 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideApi } from './api/provide-api';
+import { Configuration } from './api/configuration';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideApi(new Configuration({
+      basePath: 'http://localhost:8080',
+    }))
+  ]
 };
